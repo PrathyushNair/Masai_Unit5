f1845e555aed1d6abb3f5e279ecfc25ec9e5b062
PrathyushNair <prathyu360@gmail.com>
2022-06-22 16:14:15 +0530

    importing in all three cases

--- Week1/LEC1/index.js
+++ Week1/LEC1/index.js
@@ -1,4 +1,19 @@
+// from local system
 const {sum}=require("./test")
 const {diff}=require("./test")
 console.log(sum(3,2))
 console.log(diff(3,1))
+
+//from npm
+const iseven=require("is-even")
+console.log(iseven(7))
+
+//from coremodules
+
+const text=require("fs")
+console.log(text.readFileSync("./hello.txt",{encoding:"utf-8"}))
+
+const write=require("fs")
+console.log(write.writeFileSync("write.txt","write",{encoding:"utf-8"}))
+
+
--- Week1/LEC1/lec1.js
+++ Week1/LEC1/lec1.js
@@ -6,4 +6,3 @@ console.log(text.readFileSync("./hello.txt",{encoding:"utf-8"}))
 
 const write=require("fs")
 console.log(write.writeFileSync("write.txt","write",{encoding:"utf-8"}))
-console.log(write.writeFileSync("write/write.txt","write",{encoding:"utf-8"})) //this does not work.
