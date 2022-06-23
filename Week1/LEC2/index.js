c32372c3f6ec8dc094031be22c1b06dfc11496a2
PrathyushNair <prathyu360@gmail.com>
2022-06-23 16:13:22 +0530

    lec 2

--- Week1/LEC2/index.js
+++ Week1/LEC2/index.js
@@ -1,1 +1,21 @@
-console.log("hello world")
+const http=require("http")
+const fs=require("fs")
+const server=http.createServer((req,res)=>{
+   if(req.url==="/quiz")
+   {
+    res.end("quiz page")
+   }
+   if(req.url==="/file")
+   {
+     fs.readFile("./text.txt",{encoding:"utf-8"},(err,data)=>{
+         return res.end(data)
+        // console.log(data)
+     })
+   }
+    // res.write("hey1")
+    // res.write("hey3")
+    // res.end("hello world")
+})
+server.listen(8080,()=>{
+    console.log("listening on 8080")
+})
