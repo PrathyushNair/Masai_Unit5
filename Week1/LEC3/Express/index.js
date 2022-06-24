36dbe98a4a6d7e127746071b356bfa15fd936002
PrathyushNair <prathyu360@gmail.com>
2022-06-24 17:23:40 +0530

    delete req added

--- Week1/LEC3/Express/index.js
+++ Week1/LEC3/Express/index.js
@@ -5,10 +5,15 @@ const app=express()
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(express.text())
+
+//GET request
 app.get("/",(req,resp)=>{
     resp.send("<h3 >Hello i am a heading</h3>")
 })
+/////////////////////////////////////
 
+
+//POST request
 app.post("/send",(req,resp)=>{
     console.log(req.body)
     const datafromclient=req.body
@@ -28,6 +33,61 @@ app.post("/send",(req,resp)=>{
    
     resp.send("something was send")
 })
+/////////////////////////////////////
+
+//GET from a json server
+app.get("/messages",(req,res)=>{
+    fs.readFile("./data.json","utf-8",(err,data)=>{
+        if(err)
+        {
+            res.send("something went wrong")
+        }
+        const parseddt=JSON.parse(data)
+        const msg=parseddt.messages
+        res.send(JSON.stringify(msg))
+    })
+})
+
+
+//DELETE
+
+app.delete("/messages/:id",(req,res)=>{
+    const {id}=req.params
+    
+    fs.readFile("./data.json","utf-8",(err,data)=>{
+        if(err)
+        {
+            res.send("something went wrong")
+        }
+        const parseddata=JSON.parse(data)
+        const finaldata=parseddata.messages
+        console.log(finaldata)
+        const newdata=finaldata.filter((el)=>el.id!=id
+        )
+        console.log(newdata)
+        parseddata.messages=newdata
+        fs.writeFile("./data.json",JSON.stringify(parseddata),"utf-8",()=>{
+            res.send("data will be deleted")
+        })
+        
+    })
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 app.listen(8080,()=>{
     console.log("started server")
 })
