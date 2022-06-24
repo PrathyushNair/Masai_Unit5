0302697b4c3753f6a8c3e444b096713d135c8fbd
PrathyushNair <prathyu360@gmail.com>
2022-06-24 15:06:15 +0530

    started express

--- Week1/LEC3/Express/index.js
+++ Week1/LEC3/Express/index.js
@@ -1,1 +1,9 @@
 // console.log("hello from expres")
+const express=require("express")
+const app=express()
+app.get("/",(req,resp)=>{
+    resp.send("<h3 >Hello i am a heading</h3>")
+})
+app.listen(8080,()=>{
+    console.log("started server")
+})
